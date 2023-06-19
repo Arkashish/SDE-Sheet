49558a348eef999278caafa50d2d4b4485185f18
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-06-19 22:52:55 +0530

    Added codes for Palindrome Partitioning

---
+++ palindromepartitioning.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool ispalin(string s, int start, int end)
+{
+    while (start <= end)
+    {
+        if (s[start++] != s[end--])
+            return false;
+    }
+    return true;
+}
+void func(int idx, string s, vector<string> &path, vector<vector<string>> &ans)
+{
+    if (idx == s.size())
+    {
+        ans.push_back(path);
+        return;
+    }
+    for (int i = idx; i < s.size(); i++)
+    {
+        if (ispalin(s, idx, i))
+        {
+            path.push_back(s.substr(idx, i - idx + 1));
+            func(i + 1, s, path, ans);
+            path.pop_back();
+        }
+    }
+}
+vector<vector<string>> partition(string &s)
+{
+    // Write your code here.
+    vector<vector<string>> ans;
+    vector<string> path;
+    func(0, s, path, ans);
+    return ans;
+}
