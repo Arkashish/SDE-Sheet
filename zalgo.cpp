6698179acff32cd1800cd0262437c3f4d2ab3e70
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-07-01 22:04:10 +0530

    Added 2 versions of code for Z algorithm

--- zalgo.cpp
+++ zalgo.cpp
@@ -1,5 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// brute
+int zAlgorithm(string s, string p, int n, int m)
+{
+    // Write your code here
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == p[0])
+        {
+            if (s.substr(i, m) == p)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+// Optimised ALgo
 vector<int> computeLPS(string p, int m)
 {
     int i = 1,
