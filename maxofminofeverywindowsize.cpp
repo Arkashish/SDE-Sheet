13046eac65216478969c613df8c3c2d6e98f3ac1
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-07-03 09:44:17 +0530

    Added codes for Maximum of minimum of every window size

---
+++ maxofminofeverywindowsize.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> maxMinWindow(vector<int> a, int n)
+{
+    // Write your code here.
+    vector<int> nse(n), pse(n), ans(n, INT_MIN);
+    for (int i = 0; i < n; i++)
+    {
+        pse[i] = i;
+        nse[i] = n - i - 1;
+    }
+
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && a[st.top()] > a[i])
+        {
+            nse[st.top()] = i - st.top() - 1;
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    while (!st.empty())
+        st.pop();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && a[st.top()] > a[i])
+        {
+            pse[st.top()] = st.top() - i - 1;
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int d = nse[i] + pse[i];
+        ans[d] = max(ans[d], a[i]);
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+        if (ans[i] < ans[i + 1])
+            ans[i] = ans[i + 1];
+
+    return ans;
+}
