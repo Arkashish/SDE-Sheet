4105df9bb637356b9f4f5a1a343bd5ee4a0fefbe
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-07-05 09:50:07 +0530

    Added codes for Diameter of a Binary Tree

---
+++ diameterofbt.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int solve(TreeNode<int> *root, int &maxi)
+{
+    if (!root)
+        return 0;
+    int lh = solve(root->left, maxi);
+    int rh = solve(root->right, maxi);
+    maxi = max(maxi, lh + rh);
+    return 1 + max(lh, rh);
+}
+int diameterOfBinaryTree(TreeNode<int> *root)
+{
+    // Write Your Code Here.
+    if (!root)
+        return 0;
+    int maxi = 0;
+    solve(root, maxi);
+    return maxi;
+}
